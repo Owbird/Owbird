ff6cf02faa4df3f54e7ba0cbba7fc4e49c10ada7
owbird <owbirdphorccuo@gmail.com>
2024-02-27 15:25:54 +0000

    fix (Projects): Fix One Dev image preview

--- next.config.js
+++ next.config.js
@@ -20,6 +20,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+        {
+        protocol: "https",
+        hostname: "camo.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
