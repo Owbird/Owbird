04db2140c9716a67dab2fc01ac8651ce046ac29a
owbird <owbirdphorccuo@gmail.com>
2024-02-27 15:14:01 +0000

    chore: Remove hero text loop

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,28 +1,5 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
-type TextLoopProps = {
-  children?: (string | JSX.Element)[] | undefined;
-  interval: number | number[];
-  delay: number;
-  adjustingSpeed: number;
-  springConfig: {
-    stiffness: number;
-    damping: number;
-  };
-  fade: boolean;
-  mask: boolean;
-  noWrap: boolean;
-  className?: string;
-  onChange?: Function;
-};
-
-const TextLoop = dynamic<TextLoopProps>(
-  () => import("react-text-loop-next").then((data) => data.TextLoop),
-  { ssr: false },
-);
-
 const Hero = () => {
   return (
     <div
@@ -37,27 +14,6 @@ const Hero = () => {
         <div className="table-cell">
           <div className="container">
             <h1 className="hero-title mb-4">I am Obed.</h1>
-
-            <p className="hero-subtitle">
-              I am a{" "}
-              <TextLoop
-                interval={500}
-                delay={1000}
-                adjustingSpeed={200}
-                springConfig={{ stiffness: 100, damping: 10 }}
-                fade={true}
-                mask={false}
-                noWrap={true}
-              >
-                {[
-                  "Backend Developer",
-                  "Mobile App Developer",
-                  "Desktop App Developer",
-                ].map((item) => (
-                  <span key={item}>{item}</span>
-                ))}
-              </TextLoop>
-            </p>
           </div>
         </div>
       </div>
