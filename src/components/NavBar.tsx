6a9a7a71c3854ecdf06bf02a857d89ef0f62d129
owbird <owbirdphorccuo@gmail.com>
2024-05-11 07:44:25 +0000

    fix (Navbar): fix no gap in elements

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import WhiteButton from "./WhiteButton";
 
 const NavBar = () => {
   return (
-    <div className="flex">
+    <div className="flex gap-3">
       {navLinks.map(({ label, href }) => (
         <WhiteButton key={href} label={label} href={href} />
       ))}
