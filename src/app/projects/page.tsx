51268c17f63cca31f7c48dc343b48c92c1018ac7
owbird <owbirdphorccuo@gmail.com>
2024-06-14 20:46:55 +0000

    chore: Remove project page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,6 +5,7 @@ import clsx from "clsx";
 import { Metadata } from "next";
 import Link from "next/link";
 import { FaLink } from "react-icons/fa";
+import WhiteButton from "@/components/WhiteButton";
 
 export const metadata: Metadata = {
   title: "Projects | Owbird",
@@ -19,30 +20,43 @@ export default function ProjectsPage() {
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-        {projects.map(({ id, name, platforms, short_description }, index) => (
-          <div
-            key={id}
-            className={clsx(
-              "mr-2 animate__animated border border-white p-4 rounded-md",
-              index % 2 === 0
-                ? "animate__lightSpeedInRight"
-                : "animate__lightSpeedInLeft",
-            )}
-          >
-            <ProjectPlatformsBadge platforms={platforms} />
-            <div className="flex items-center hover:text-green-500">
-              <Link
-                target="_blank"
-                href={`/projects/${id}`}
-                className="font-bold text-xl mr-2"
-              >
-                {name}
-              </Link>
-              <FaLink />
+        {projects.map(
+          ({ id, name, platforms, short_description, url, github }, index) => (
+            <div
+              key={id}
+              className={clsx(
+                "mr-2 animate__animated border border-white p-4 rounded-md flex flex-col justify-between",
+                index % 2 === 0
+                  ? "animate__lightSpeedInRight"
+                  : "animate__lightSpeedInLeft",
+              )}
+            >
+              <ProjectPlatformsBadge platforms={platforms} />
+              <div className="flex items-center hover:text-green-500">
+                <Link
+                  target="_blank"
+                  href={`/projects/${id}`}
+                  className="font-bold text-xl mr-2"
+                >
+                  {name}
+                </Link>
+                <FaLink />
+              </div>
+              <p className="text-gray-300">{short_description}</p>
+
+              <div className="flex mt-4 mb-4">
+                <div className="mr-2">
+                  <WhiteButton label={"Github"} href={github} />
+                </div>
+                {github !== url && (
+                  <div className="mr-2">
+                    <WhiteButton label={"Website"} href={url} />
+                  </div>
+                )}
+              </div>
             </div>
-            <p className="text-gray-300">{short_description}</p>
-          </div>
-        ))}
+          ),
+        )}
       </div>
     </div>
   );
