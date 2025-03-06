c5b586941a75df74be213e537472c45660f6a9be
Owbird <40531477+Owbird@users.noreply.github.com>
2025-03-06 18:31:09 +0000

    chore (data): Update social links

--- src/data/constants/index.ts
+++ src/data/constants/index.ts
@@ -9,6 +9,7 @@ export const socialLinks = [
   { label: "Phone", href: "tel:233265542141" },
   { label: "Github", href: "https://github.com/owbird" },
   { label: "X (Twitter)", href: "https://x.com/_owbird_" },
+  { label: "TikTok", href: "https://www.tiktok.com/@owbird.dev" },
   { label: "LinkedIn", href: "https://www.linkedin.com/in/obed-forkuo/" },
   { label: "Blog", href: "https://blog.owbird.site/" },
 ];
